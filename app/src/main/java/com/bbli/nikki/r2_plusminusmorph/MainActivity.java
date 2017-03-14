1b146004df259a2bce039321ff1e45057ceb964a
Rumani <rumani@ssccglpinnacle.com>
2017-03-14 23:09:13 +0530

    Changes done in Main Activity to inflate XML.

--- app/src/main/java/com/bbli/nikki/r2_plusminusmorph/MainActivity.java
+++ app/src/main/java/com/bbli/nikki/r2_plusminusmorph/MainActivity.java
@@ -2,12 +2,29 @@ package com.bbli.nikki.r2_plusminusmorph;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
+    boolean isChecked;
+    @BindView(R.id.plusminus)
+    ImageView plusMinusView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
-}
+
+        @OnClick(R.id.rootview)
+        void onClick () {
+             isChecked = !isChecked;
+            final int[] stateSet = {android.R.attr.state_checked * (isChecked ? 1 : -1)};
+            plusMinusView.setImageState(stateSet, true);
+
+        }
+    }
+
